b41f9bfe287f7e606861631084b69d9c6eea2ab5
mingli202 <vincentmingli@gmail.com>
2024-12-17 19:22:22 -0500

    refac(textbox): thread safe

--- src/screen/typing_test/textbox.rs
+++ src/screen/typing_test/textbox.rs
@@ -1,5 +1,7 @@
 use std::cmp::Ordering;
 use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Instant;
 
 use macroquad::window;
@@ -11,8 +13,10 @@ pub struct TextBoxState<'a> {
     pub letters: Vec<Letter>,
     pub index: usize,
     pub time_started: Instant,
+    pub timer: Instant,
     pub started: bool,
     pub data: &'a Data,
+    pub incremental_wpm: Arc<Mutex<Vec<u16>>>,
 }
 
 pub struct TextBox<'a> {
@@ -30,33 +34,33 @@ impl<'a> TextBox<'a> {
             .enumerate()
             .map(|(id, c)| Letter {
                 letter: c,
-                color: Rc::clone(&style.theme.ghost),
+                color: Arc::clone(&style.theme.ghost),
                 id,
             })
             .collect();
 
-        let f1 = Rc::clone(&style.font_size);
-        let f2 = Rc::clone(&style.font_size);
+        let f1 = Arc::clone(&style.font_size);
+        let f2 = Arc::clone(&style.font_size);
 
         TextBox {
             style: Style {
-                font_size: Rc::clone(&style.font_size),
+                font_size: Arc::clone(&style.font_size),
                 border: Some(BorderParams {
                     size: 2.0,
-                    color: Rc::clone(&style.theme.ghost),
+                    color: Arc::clone(&style.theme.ghost),
                 }),
                 theme: Theme {
-                    bg: Rc::clone(&style.theme.bg),
-                    ghost: Rc::clone(&style.theme.ghost),
-                    text: Rc::clone(&style.theme.text),
-                    error: Rc::clone(&style.theme.error),
+                    bg: Arc::clone(&style.theme.bg),
+                    ghost: Arc::clone(&style.theme.ghost),
+                    text: Arc::clone(&style.theme.text),
+                    error: Arc::clone(&style.theme.error),
                 },
                 x: Value::Relative(Box::new(|| (0.5 * window::screen_width()) / 2.0)),
                 y: Value::Relative(Box::new(move || {
-                    (window::screen_height() - *f1.borrow() * 3.0) / 2.0
+                    (window::screen_height() - *f1.lock().unwrap() * 3.0) / 2.0
                 })),
                 width: Value::Relative(Box::new(|| window::screen_width() / 2.0)),
-                height: Value::Relative(Box::new(move || *f2.borrow() * 3.0)),
+                height: Value::Relative(Box::new(move || *f2.lock().unwrap() * 3.0)),
                 clip: true,
                 offset_y: None,
                 offset_x: None,
@@ -67,7 +71,9 @@ impl<'a> TextBox<'a> {
                 letters,
                 index: 0,
                 time_started: Instant::now(),
+                timer: Instant::now(),
                 started: false,
+                incremental_wpm: Arc::new(Mutex::new(vec![])),
                 data,
             },
         }
@@ -80,7 +86,7 @@ impl<'a> TextBox<'a> {
             .enumerate()
             .map(|(id, c)| Letter {
                 letter: c,
-                color: Rc::clone(&self.style.theme.ghost),
+                color: Arc::clone(&self.style.theme.ghost),
                 id,
             })
             .collect();
@@ -99,12 +105,12 @@ impl<'a> TextBox<'a> {
 
         if c == self.state.letters[self.state.index].letter {
             self.state.letters[self.state.index] = Letter {
-                color: Rc::clone(&self.style.theme.text),
+                color: Arc::clone(&self.style.theme.text),
                 ..self.state.letters[self.state.index]
             };
         } else {
             self.state.letters[self.state.index] = Letter {
-                color: Rc::clone(&self.style.theme.error),
+                color: Arc::clone(&self.style.theme.error),
                 ..self.state.letters[self.state.index]
             };
         }
@@ -119,7 +125,7 @@ impl<'a> TextBox<'a> {
         }
         self.state.index -= 1;
         self.state.letters[self.state.index] = Letter {
-            color: Rc::clone(&self.style.theme.ghost),
+            color: Arc::clone(&self.style.theme.ghost),
             ..self.state.letters[self.state.index]
         };
     }
@@ -144,8 +150,40 @@ impl<'a> TextBox<'a> {
             left -= 1;
         }
         self.style.offset_y = Some(Value::Absolute(
-            -(left as f32 * *self.style.font_size.borrow()),
+            -(left as f32 * *self.style.font_size.lock().unwrap()),
         ));
+    }
+
+    pub fn get_incremental_wpm(&mut self) {
+        let t = self.state.timer.elapsed().as_millis();
+        if !self.state.started || t < 500 {
+            return;
+        }
+
+        let v = Arc::clone(&self.state.incremental_wpm);
+
+        //thread::spawn(move || {
+        //    let mut wrongs = 0.0;
+        //    let mut is_word_wrong = false;
+        //
+        //    for letter in &self.state.letters {
+        //        if *letter.color.lock().unwrap() == *self.style.theme.error.lock().unwrap()
+        //            && !is_word_wrong
+        //        {
+        //            wrongs += 1.0;
+        //            is_word_wrong = true;
+        //        }
+        //        if letter.letter == ' ' {
+        //            is_word_wrong = false;
+        //        }
+        //    }
+        //
+        //    let w = 1000 * 60 * (self.state.letters.len() as f32 / 5.0 - wrongs) as u128 / t;
+        //
+        //    let v = v.lock().unwrap();
+        //});
+
+        self.state.timer = Instant::now();
     }
 
     pub fn get_wpm(&self) -> u16 {
@@ -154,7 +192,9 @@ impl<'a> TextBox<'a> {
         let mut is_word_wrong = false;
 
         for letter in &self.state.letters {
-            if *letter.color.borrow() == *self.style.theme.error.borrow() && !is_word_wrong {
+            if *letter.color.lock().unwrap() == *self.style.theme.error.lock().unwrap()
+                && !is_word_wrong
+            {
                 wrongs += 1.0;
                 is_word_wrong = true;
             }
@@ -165,6 +205,7 @@ impl<'a> TextBox<'a> {
 
         (1000 * 60 * (self.state.letters.len() as f32 / 5.0 - wrongs) as u128 / time_passed) as u16
     }
+
     pub fn update(&mut self) {
         self.style.draw_bg();
 
@@ -177,6 +218,7 @@ impl<'a> TextBox<'a> {
         if self.state.index > 0 && !self.state.started {
             self.state.started = true;
             self.state.time_started = Instant::now();
+            self.state.timer = Instant::now();
         }
     }
 }
