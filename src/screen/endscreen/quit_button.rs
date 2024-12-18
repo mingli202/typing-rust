7f12e793efce3e9cb02f153770523c3a4ffeaf47
mingli202 <vincentmingli@gmail.com>
2024-12-17 21:24:10 -0500

    fix(quit_button): mutex lock freeze

--- src/screen/endscreen/quit_button.rs
+++ src/screen/endscreen/quit_button.rs
@@ -27,10 +27,12 @@ impl QuitButton {
                     color: Arc::clone(&style.theme.text),
                 }),
                 x: Value::Relative(Box::new(move || {
-                    (window::screen_width() + *f1.lock().unwrap()) / 2.0
+                    let f1 = *f1.lock().unwrap();
+                    (window::screen_width() + f1) / 2.0
                 })),
                 y: Value::Relative(Box::new(move || {
-                    (window::screen_height() + *font_size.lock().unwrap()) / 2.0
+                    let font_size = *font_size.lock().unwrap();
+                    (window::screen_height() + font_size) / 2.0
                 })),
                 font_size: Arc::clone(&style.font_size),
                 theme: Theme {
@@ -42,12 +44,12 @@ impl QuitButton {
                 padding_x: Some(Value::Absolute(10.0)),
                 padding_y: Some(Value::Absolute(10.0)),
                 width: Value::Relative(Box::new(move || {
-                    text::measure_text("Next (n)", None, *f3.lock().unwrap() as u16, 1.0).width
-                        + 20.0
+                    let f3 = *f3.lock().unwrap();
+                    text::measure_text("Next (n)", None, f3 as u16, 1.0).width + 20.0
                 })),
                 height: Value::Relative(Box::new(move || {
-                    text::measure_text("Next (n)", None, *f4.lock().unwrap() as u16, 1.0).height
-                        + 20.0
+                    let f4 = *f4.lock().unwrap();
+                    text::measure_text("Next (n)", None, f4 as u16, 1.0).height + 20.0
                 })),
                 ..Style::default()
             },
