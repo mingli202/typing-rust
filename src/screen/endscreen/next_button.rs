c1f16b4c11769be69a3899aa3f81fa9e23c813b2
mingli202 <vincentmingli@gmail.com>
2024-12-17 19:08:44 -0500

    refac(next_button): thread safe

--- src/screen/endscreen/next_button.rs
+++ src/screen/endscreen/next_button.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::sync::Arc;
 
 use macroquad::{text, window};
 
@@ -13,40 +13,42 @@ impl NextButton {
     pub fn new(style: &Style) -> NextButton {
         let text = "Next (n)".to_string();
 
-        let f1 = Rc::clone(&style.font_size);
-        let f2 = Rc::clone(&style.font_size);
-        let f3 = Rc::clone(&style.font_size);
-        let f4 = Rc::clone(&style.font_size);
+        let f1 = Arc::clone(&style.font_size);
+        let f2 = Arc::clone(&style.font_size);
+        let f3 = Arc::clone(&style.font_size);
+        let f4 = Arc::clone(&style.font_size);
 
         NextButton {
             text: text.to_string(),
             style: Style {
                 border: Some(BorderParams {
                     size: 2.0,
-                    color: Rc::clone(&style.theme.text),
+                    color: Arc::clone(&style.theme.text),
                 }),
                 x: Value::Relative(Box::new(move || {
-                    (window::screen_width() - *f1.borrow()) / 2.0
-                        - text::measure_text(&text, None, *f1.borrow() as u16, 1.0).width
+                    (window::screen_width() - *f1.lock().unwrap()) / 2.0
+                        - text::measure_text(&text, None, *f1.lock().unwrap() as u16, 1.0).width
                         - 20.0
                 })),
                 y: Value::Relative(Box::new(move || {
-                    (window::screen_height() + *f2.borrow()) / 2.0
+                    (window::screen_height() + *f2.lock().unwrap()) / 2.0
                 })),
-                font_size: Rc::clone(&style.font_size),
+                font_size: Arc::clone(&style.font_size),
                 theme: Theme {
-                    bg: Rc::clone(&style.theme.bg),
-                    ghost: Rc::clone(&style.theme.ghost),
-                    text: Rc::clone(&style.theme.ghost),
-                    error: Rc::clone(&style.theme.error),
+                    bg: Arc::clone(&style.theme.bg),
+                    ghost: Arc::clone(&style.theme.ghost),
+                    text: Arc::clone(&style.theme.ghost),
+                    error: Arc::clone(&style.theme.error),
                 },
                 padding_x: Some(Value::Absolute(10.0)),
                 padding_y: Some(Value::Absolute(10.0)),
                 width: Value::Relative(Box::new(move || {
-                    text::measure_text("Next (n)", None, *f3.borrow() as u16, 1.0).width + 20.0
+                    text::measure_text("Next (n)", None, *f3.lock().unwrap() as u16, 1.0).width
+                        + 20.0
                 })),
                 height: Value::Relative(Box::new(move || {
-                    text::measure_text("Next (n)", None, *f4.borrow() as u16, 1.0).height + 20.0
+                    text::measure_text("Next (n)", None, *f4.lock().unwrap() as u16, 1.0).height
+                        + 20.0
                 })),
                 ..Style::default()
             },
