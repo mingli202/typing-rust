2feae41e21e51b2715ad40ceaf8efc48ac01bc88
mingli202 <vincentmingli@gmail.com>
2025-02-22 20:11:29 -0500

    refactor(typing test): array of letters to array of words
    by refactoring from an array of letter to an array of words, we can
    mimic more closely to the behavior of monkeytype, which pressing space
    will move the next word and words can overflow

--- src/app/typing_test/textbox.rs
+++ src/app/typing_test/textbox.rs
@@ -2,13 +2,18 @@ use std::cmp::Ordering;
 use std::rc::Rc;
 use std::time::Instant;
 
-use macroquad::window;
+use macroquad::text::TextDimensions;
+use macroquad::{shapes, text, window};
 
-use crate::app::{self, theme::Theme, BorderParams, Letter, Style, Value};
+use crate::app::Word;
+use crate::app::{theme::Theme, BorderParams, Letter, Style, Value};
 
 pub struct TextBoxState {
     pub letters: Vec<Letter>,
+    pub words: Vec<Word>,
     pub index: usize,
+    pub word_index: usize,
+    pub char_index: usize,
     pub time_started: Instant,
     pub started: bool,
 }
@@ -26,9 +31,21 @@ impl TextBox {
             .map(|(id, c)| Letter {
                 letter: c,
                 color: Rc::clone(&style.theme.ghost),
-                id,
+                char_id: id,
+                word_id: id,
             })
             .collect();
+
+        let words: Vec<Word> = text
+            .split(" ")
+            .enumerate()
+            .map(|(id, word)| Word::from_str(style, word, id))
+            .collect();
+
+        /*  0        1        2       3     4      5       6        7
+         * [[hello], [world], [this], [is], [the], [best], [thing], [ever]]
+         *   01234    01234    0123    01    012    0123    01234    0123
+         * */
 
         let f1 = Rc::clone(&style.font_size);
         let f2 = Rc::clone(&style.font_size);
@@ -58,8 +75,11 @@ impl TextBox {
             state: TextBoxState {
                 letters,
                 index: 0,
+                word_index: 0,
+                char_index: 0,
                 time_started: Instant::now(),
                 started: false,
+                words,
             },
         }
     }
@@ -71,7 +91,8 @@ impl TextBox {
             .map(|(id, c)| Letter {
                 letter: c,
                 color: Rc::clone(&self.style.theme.ghost),
-                id,
+                char_id: id,
+                word_id: id,
             })
             .collect();
 
@@ -83,35 +104,70 @@ impl TextBox {
     }
 
     pub fn on_type(&mut self, c: char) -> bool {
-        if self.state.index == self.state.letters.len() - 1 {
+        if self.state.word_index == self.state.words.len() - 1
+            && self.state.char_index == self.state.words.last().unwrap().letters.len() - 1
+        {
             return true;
         }
 
-        if c == self.state.letters[self.state.index].letter {
-            self.state.letters[self.state.index] = Letter {
-                color: Rc::clone(&self.style.theme.text),
-                ..self.state.letters[self.state.index]
-            };
-        } else {
-            self.state.letters[self.state.index] = Letter {
-                color: Rc::clone(&self.style.theme.error),
-                ..self.state.letters[self.state.index]
-            };
+        if c == ' ' {
+            self.state.word_index += 1;
+            self.state.char_index = 0;
+            return false;
         }
 
-        self.state.index += 1;
+        if self.state.char_index == self.state.words[self.state.word_index].letters.len() {
+            self.state.words[self.state.word_index]
+                .letters
+                .push(Letter {
+                    letter: c,
+                    color: Rc::clone(&self.style.theme.error),
+                    char_id: self.state.char_index,
+                    word_id: self.state.word_index,
+                });
+            self.state.char_index += 1;
+            return false;
+        }
+
+        let correct = self.state.words[self.state.word_index].letters[self.state.char_index].letter;
+
+        self.state.words[self.state.word_index].letters[self.state.char_index] = if c == correct {
+            Letter {
+                color: Rc::clone(&self.style.theme.text),
+                ..(self.state.words[self.state.word_index].letters[self.state.char_index]).clone()
+            }
+        } else {
+            Letter {
+                color: Rc::clone(&self.style.theme.error),
+                ..(self.state.words[self.state.word_index].letters[self.state.char_index]).clone()
+            }
+        };
+
+        self.state.char_index += 1;
         false
     }
 
     pub fn delete_char(&mut self) {
-        if self.state.index == 0 {
+        if self.state.char_index == 0 {
+            if self.state.word_index == 0 {
+                return;
+            }
+
+            self.state.word_index -= 1;
+            self.state.char_index = self.state.words[self.state.word_index].letters.len();
             return;
         }
-        self.state.index -= 1;
-        self.state.letters[self.state.index] = Letter {
-            color: Rc::clone(&self.style.theme.ghost),
-            ..self.state.letters[self.state.index]
-        };
+
+        self.state.char_index -= 1;
+
+        if self.state.char_index >= self.state.words[self.state.word_index].word.len() {
+            self.state.words[self.state.word_index].letters.pop();
+        } else {
+            self.state.words[self.state.word_index].letters[self.state.char_index] = Letter {
+                color: Rc::clone(&self.style.theme.ghost),
+                ..(self.state.words[self.state.word_index].letters[self.state.char_index]).clone()
+            };
+        }
     }
 
     fn update_position(&mut self, line_breaks: &[usize]) {
@@ -121,7 +177,7 @@ impl TextBox {
         while left < right {
             let mid = (left + right) / 2;
 
-            match self.state.index.cmp(&line_breaks[mid as usize]) {
+            match self.state.word_index.cmp(&line_breaks[mid as usize]) {
                 Ordering::Less => right = mid - 1,
                 Ordering::Greater => left = mid + 1,
                 Ordering::Equal => {
@@ -130,7 +186,7 @@ impl TextBox {
                 }
             }
         }
-        if left > 0 && self.state.index < line_breaks[left as usize] {
+        if left > 0 && self.state.word_index < line_breaks[left as usize] {
             left -= 1;
         }
         self.style.offset_y = Some(Value::Absolute(
@@ -160,8 +216,9 @@ impl TextBox {
     pub fn update(&mut self) {
         self.style.draw_bg();
 
-        let line_breaks =
-            app::text::print_letters_wrap(&self.style, &self.state.letters, self.state.index);
+        //let line_breaks =
+        //    app::text::print_letters_wrap(&self.style, &self.state.letters, self.state.index);
+        let line_breaks = self.print_words();
         self.update_position(&line_breaks);
 
         self.style.draw_mask();
@@ -170,5 +227,168 @@ impl TextBox {
             self.state.started = true;
             self.state.time_started = Instant::now();
         }
+    }
+
+    fn print_words(&self) -> Vec<usize> {
+        let mut line_breaks = vec![];
+
+        let mut lines = 0;
+
+        let mut line: Vec<&Word> = vec![];
+
+        let p_x = match &self.style.padding_x {
+            Some(p) => p.get(&self.style),
+            _ => 0.0,
+        };
+
+        for word in &self.state.words {
+            let l = line
+                .iter()
+                .map(|w| {
+                    w.letters
+                        .iter()
+                        .fold(String::new(), |acc, l| acc + &l.letter.to_string())
+                })
+                .collect::<Vec<String>>()
+                .join(" ")
+                + " "
+                + &word.word;
+
+            let TextDimensions { width, .. } =
+                text::measure_text(&l, None, *self.style.font_size.borrow() as u16, 1.0);
+
+            if width > self.style.width.get(&self.style) - 2.0 * p_x {
+                self.print_letters(
+                    &line,
+                    self.style.x.get(&self.style),
+                    self.style.y.get(&self.style) + lines as f32 * *self.style.font_size.borrow(),
+                    self.state.word_index,
+                    self.state.char_index,
+                );
+
+                line_breaks.push(word.id);
+                lines += 1;
+                line = vec![word];
+            } else {
+                line.push(word);
+            }
+        }
+
+        self.print_letters(
+            &line,
+            self.style.x.get(&self.style),
+            self.style.y.get(&self.style) + lines as f32 * *self.style.font_size.borrow(),
+            self.state.word_index,
+            self.state.char_index,
+        );
+
+        line_breaks
+    }
+
+    fn print_letters(&self, line: &[&Word], x: f32, y: f32, word_index: usize, char_index: usize) {
+        let mut letters: Vec<Letter> = vec![];
+
+        for word in line {
+            for letter in &word.letters {
+                letters.push(letter.clone());
+            }
+            letters.push(Letter {
+                letter: ' ',
+                color: Rc::clone(&self.style.theme.ghost),
+                char_id: word.letters.len(),
+                word_id: word.id,
+            })
+        }
+
+        letters.pop();
+
+        let mut offset_x = 0.0;
+        let offset_y = text::measure_text(
+            &letters
+                .iter()
+                .fold(String::new(), |acc, l| acc + &l.letter.to_string()),
+            None,
+            *self.style.font_size.borrow() as u16,
+            1.0,
+        )
+        .offset_y;
+
+        for letter in &letters {
+            offset_x += self
+                .print_letter(letter, x + offset_x, y + offset_y, word_index, char_index)
+                .width;
+        }
+    }
+
+    fn print_letter(
+        &self,
+        letter: &Letter,
+        x: f32,
+        y: f32,
+        word_index: usize,
+        char_index: usize,
+    ) -> TextDimensions {
+        let style = &self.style;
+
+        let dimensions = text::measure_text(
+            &letter.letter.to_string(),
+            None,
+            *style.font_size.borrow() as u16,
+            1.0,
+        );
+
+        let p_x = match &style.padding_x {
+            Some(p) => p.get(style),
+            _ => 0.0,
+        };
+
+        let o_x = match &style.offset_x {
+            Some(p) => p.get(style),
+            _ => 0.0,
+        };
+
+        let p_y = match &style.padding_y {
+            Some(p) => p.get(style),
+            _ => 0.0,
+        };
+
+        let o_y = match &style.offset_y {
+            Some(p) => p.get(style),
+            _ => 0.0,
+        };
+
+        let x = x + p_x + o_x;
+        let y = y + p_y + o_y;
+
+        text::draw_text(
+            &letter.letter.to_string(),
+            x,
+            y,
+            *style.font_size.borrow(),
+            *letter.color.borrow(),
+        );
+
+        if *letter.color.borrow() == *style.theme.error.borrow() {
+            shapes::draw_line(
+                x,
+                y + 0.2 * *style.font_size.borrow(),
+                x + dimensions.width,
+                y + 0.2 * *style.font_size.borrow(),
+                0.05 * *style.font_size.borrow(),
+                *letter.color.borrow(),
+            );
+        }
+
+        if letter.char_id == char_index && letter.word_id == word_index {
+            text::draw_text(
+                "|",
+                x - dimensions.width / 2.0,
+                y,
+                *style.font_size.borrow(),
+                *style.theme.text.borrow(),
+            );
+        }
+
+        dimensions
     }
 }
