687e8b6bb94b82152f59d446077d10cd82987582
mingli202 <vincentmingli@gmail.com>
2025-02-28 16:03:48 -0500

    feat: linespace type shit

--- src/app/endscreen/graph.rs
+++ src/app/endscreen/graph.rs
@@ -1,16 +1,19 @@
+use std::process;
 use std::rc::Rc;
 use std::time::Duration;
 
 use macroquad::color::Color;
-use macroquad::text::Font;
+use macroquad::text::{self, Font};
 use macroquad::{shapes, window};
 
 use crate::app::style::Style;
+use crate::app::text::PrintOptions;
 use crate::app::theme::Theme;
-use crate::app::{util, Value};
+use crate::app::{self, util, Value};
 
 pub struct Graph {
-    incremental_wpm: Vec<u16>,
+    incremental_wpm: Vec<(Duration, u16)>,
+    max_wpm: u16,
     time: Duration,
     wrongs: i32,
     pub style: Style,
@@ -20,12 +23,14 @@ pub struct Graph {
 impl Graph {
     pub fn new(
         style: &Style,
-        incremental_wpm: Vec<u16>,
+        incremental_wpm: Vec<(Duration, u16)>,
+        max_wpm: u16,
         time: Duration,
         wrongs: i32,
         font: Rc<Font>,
     ) -> Self {
         Graph {
+            max_wpm,
             font,
             incremental_wpm,
             time,
@@ -61,8 +66,56 @@ impl Graph {
             self.style.y.get(parent),
             self.style.width.get(&self.style),
             self.style.height.get(&self.style),
-            1.0,
+            2.0,
             Color::new(0.0, 1.0, 0.0, 1.0),
         );
+
+        let x = self.style.x();
+        let y = self.style.y();
+        let fsize = *self.style.font_size.borrow();
+
+        let wpm_range = Self::range(0.0, self.max_wpm as f32, 5);
+        let wpm_y_range = Self::range(y, y + self.style.height() - 2.0 * fsize, 5);
+
+        for (_wpm, _y) in wpm_range.iter().rev().zip(wpm_y_range) {
+            app::text::print_text(
+                &self.style,
+                &format!("{}", _wpm),
+                PrintOptions {
+                    x: Some(x),
+                    y: Some(_y),
+                    font: Some(Rc::clone(&self.font)),
+                    font_scale: Some(1.0),
+                    color: Some(*self.style.theme.ghost.borrow()),
+                    ..PrintOptions::default()
+                },
+            );
+        }
+    }
+
+    fn range(start: f32, end: f32, n_steps: usize) -> Vec<f32> {
+        let mut o = vec![];
+
+        for i in 0..=n_steps {
+            o.push(start + (end - start) * i as f32 / n_steps as f32);
+        }
+
+        o
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn range_1() {
+        let arr = Graph::range(1.0, 4.0, 3);
+        assert_eq!(arr, vec![1.0, 2.0, 3.0, 4.0]);
+    }
+    #[test]
+    fn range_2() {
+        let arr = Graph::range(100.0, 400.0, 3);
+        assert_eq!(arr, vec![100.0, 200.0, 300.0, 400.0]);
     }
 }
