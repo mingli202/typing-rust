a0651e51523cc2cb62b6667e9b0e99042895c58f
mingli202 <vincentmingli@gmail.com>
2025-04-20 20:52:42 -0400

    removed unused files

--- src/app/bombparty/mod.rs
+++ src/app/bombparty/mod.rs
@@ -1,47 +1,31 @@
-use std::rc::Rc;
-use std::sync::mpsc;
+use macroquad::{input, window};
 
-use macroquad::window;
-
-use self::component::{Button1, Component};
+use self::component::Component;
 
 use super::{util, App};
 
 mod component;
-mod macros;
-mod state;
 
 pub async fn run(app: &mut App) {
-    let mut state = State::default();
-    let (tx, rx) = mpsc::channel::<StateAction>();
-
-    let mut components: Vec<Box<dyn Component>> = vec![Button1::new(
-        &app.style,
-        Rc::clone(&app.font),
-        state.counter,
-        tx.clone(),
-    )];
+    let mut components: Vec<Box<dyn Component>> = vec![];
 
     loop {
         window::clear_background(*app.style.theme.bg.borrow());
+
+        let is_mouse_pressed = input::is_mouse_button_pressed(input::MouseButton::Left);
 
         for component in components.iter_mut() {
             component.as_mut().refresh();
 
             if util::is_hover(component.get_style()) {
                 component.onhover();
+
+                if is_mouse_pressed {
+                    component.onclick();
+                }
             }
         }
 
         window::next_frame().await;
     }
-}
-
-#[derive(Debug, Default)]
-struct State {
-    counter: i32,
-}
-
-enum StateAction {
-    ButtonCounterInc { inc: i32 },
 }
