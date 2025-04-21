40f24942c8440ed58c9b5b6c7973a4c3c0ca1a65
mingli202 <vincentmingli@gmail.com>
2025-04-21 07:44:13 -0400

    feat(bombparty): new Style struct

---
+++ src/app/bombparty/style.rs
@@ -0,0 +1,38 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use macroquad::text::Font;
+
+use crate::app::theme::Theme;
+use crate::app::{self, BorderParams};
+
+#[derive(Debug, Clone, Default)]
+pub struct Style {
+    pub theme: Theme,
+    pub font: Option<Rc<Font>>,
+    pub font_size: Rc<RefCell<f32>>,
+    pub border: Option<BorderParams>,
+    pub clip: bool,
+    pub wrap: bool,
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Style {
+    pub fn from(style: &app::Style) -> Self {
+        Style {
+            theme: style.copy_theme(),
+            font: style.font.as_ref().map(Rc::clone),
+            font_size: Rc::clone(&style.font_size),
+            ..Self::default()
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Val {
+    Abs(f32),
+    Percent(f32),
+}
