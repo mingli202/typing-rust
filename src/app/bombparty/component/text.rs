323b42171d1e3aa7d04163c9d52a3a1a916bfdc8
mingli202 <vincentmingli@gmail.com>
2025-04-21 09:05:42 -0400

    feat(component): multiline

--- src/app/bombparty/component/text.rs
+++ src/app/bombparty/component/text.rs
@@ -1,4 +1,4 @@
-use macroquad::text::{self, TextParams};
+use macroquad::text::{self, TextDimensions, TextParams};
 
 use crate::app::bombparty::Style;
 
@@ -9,6 +9,12 @@ pub struct Text {
     pub style: Style,
 }
 
+impl Text {
+    pub fn new(style: Style, text: String) -> Self {
+        Text { style, text }
+    }
+}
+
 impl Component for Text {
     fn get_style_mut(&mut self) -> &mut Style {
         &mut self.style
@@ -17,16 +23,44 @@ impl Component for Text {
         &self.style
     }
     fn refresh(&mut self) {
-        text::draw_text_ex(
-            &self.text[..],
-            self.style.x,
-            self.style.y,
-            TextParams {
-                color: *self.style.theme.text.borrow(),
-                font: self.style.font.as_deref(),
-                font_size: *self.style.font_size.borrow() as u16,
-                ..TextParams::default()
-            },
-        );
+        let txt = self.text.replace("\t", "    ");
+        let lines = txt.split("\n");
+
+        let mut max_width = 0.0;
+        let mut height = 0.0;
+
+        let fsize = *self.style.font_size.borrow();
+
+        for line in lines {
+            let TextDimensions {
+                width: w, offset_y, ..
+            } = text::measure_text(
+                line,
+                self.style.font.as_deref(),
+                *self.style.font_size.borrow() as u16,
+                1.0,
+            );
+
+            text::draw_text_ex(
+                line,
+                self.style.x,
+                self.style.y + height + offset_y,
+                TextParams {
+                    color: *self.style.theme.text.borrow(),
+                    font: self.style.font.as_deref(),
+                    font_size: *self.style.font_size.borrow() as u16,
+                    ..TextParams::default()
+                },
+            );
+
+            height += fsize;
+
+            if w > max_width {
+                max_width = w;
+            }
+        }
+
+        self.style.width = max_width;
+        self.style.height = height;
     }
 }
